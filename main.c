c5153c9e51809e62edb03886558498368861c1b2
Nova28u <fiqrie28@gmail.com>
2017-07-31 09:48:06 +0800

    Update main.c
    added TRISE definitions

--- main.c
+++ main.c
@@ -10,11 +10,12 @@ int playsound2(void);
 //message functions declare here later
 void main(void) {
     
-ADCON1 = 0x0F; //allow usage of digital inputs for all pins
-TRISC=0b00000000; //Declare output pins for LCD
-TRISA=0b00111111; //Declare extra output pins for LCD
-TRISD=0b00000000; //Declare outputs for 7 segment
-TRISB=0b11001111;//Declare inputs from RB0 to RB3 and output from RB4 and RB5 ignore RB6 and RB7 note: RB5 is speaker
+ADCON1 = 0x0F;         //allow usage of digital inputs for all pins
+TRISA = 0b00111111;    //Declare extra output pins for LCD
+TRISB = 0b11001111;    //Declare inputs from RB0 to RB3 and output from RB4 and RB5 ignore RB6 and RB7 note: RB5 is speaker
+TRISC = 0b00000000;    //Declare output pins for LCD
+TRISD = 0b00000000;    //Declare outputs for 7 segment
+TRISE = 0b0000 0000;   //LCD selector
 char keypad_input;
 
 //"press C to fly straight"
